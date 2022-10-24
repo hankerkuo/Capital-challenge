46079fddb290740c4f0ac366576a9a4e8fd97639
Hank <hankerkuo@gmail.com>
2022-10-24 23:16:14 +0800

    add empty string condition handling for random util

--- src/utils/RandomUtil.ts
+++ src/utils/RandomUtil.ts
@@ -1,16 +1,18 @@
 class RandomUtil {
-  static getOneFromArray = <T>(array: T[]): T => {
+  // return null if array length = 0
+  static getOneFromArray = <T>(array: T[]): T | null => {
     const length = array.length;
+    if (length === 0) return null;
     const randomIdx = Math.floor(Math.random() * length);
     return array[randomIdx];
   }
   static getOneKeyValueFromObj = 
-    (target: { [key: string]: string }): { key: string; value: string } => {
+    (target: { [key: string]: string }): { key: string; value: string } | null=> {
     const randomKey = RandomUtil.getOneFromArray(Object.keys(target));
-    return {
+    return randomKey ? {
       key: randomKey,
       value: target[randomKey]
-    }
+    }: null
   }
 }
 
