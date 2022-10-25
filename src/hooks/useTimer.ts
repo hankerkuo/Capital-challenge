dc7bd560b47e02690560f34b07125b35ecec7130
Hank <hankerkuo@gmail.com>
2022-10-25 23:04:26 +0800

    add custom hook for timer

---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState, useTransition } from 'react';
+
+const useTimer = (interval :number) => {
+  const [startDate, setStartDate] = useState(new Date().getTime());
+  const [pause, setPause] = useState(true);
+  const [timer, setTimer] =
+    useState(new Date().getTime() - startDate);
+
+  const resetTimer = () => {
+    setStartDate(new Date().getTime());
+  }
+  const startTimer = () => {
+    resetTimer();
+    setPause(false);
+  }
+  const pauseTimer = () => {
+    setPause(true);
+  }
+  const resumeTimer = () => {
+    setPause(false);
+  }
+
+  useEffect(() => {
+    if(!pause) {
+      const timerInterval = setInterval(() => {
+        setTimer(new Date().getTime() - startDate);
+      }, interval);
+  
+      return () => clearInterval(timerInterval);
+    }
+  }, [startDate, pause]);
+
+  return {timer, resetTimer, startTimer, pauseTimer, resumeTimer};
+};
+
+export default useTimer
