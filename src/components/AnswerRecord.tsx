636f0025d0cc129d8ad51d4065e0e169466e68d0
Hank <hankerkuo@gmail.com>
2022-10-29 06:55:32 +0800

    add answer record styling-specific css

--- src/components/AnswerRecord.tsx
+++ src/components/AnswerRecord.tsx
@@ -6,19 +6,28 @@ const AnswerRecord = ({ answerRecord }: { answerRecord: TAnswerRecord }) => {
   return (
     <div className={styles.answerRecordTable}>
       <div className={styles.title}>
-        <div className={styles.col1Layout}>Country</div>
-        <div className={styles.col2Layout}>Capital</div>
-        <div className={styles.col3Layout}>Time spent</div>
+        <div className={`${styles.col1} ${styles.col1Layout}`}>
+          Country
+        </div>
+        <div className={`${styles.col2} ${styles.col2Layout}`}>
+          Capital
+        </div>
+        <div className={`${styles.col3} ${styles.col3Layout}`}>
+          Time spent
+        </div>
       </div>
       {answerRecord.map(singleAnswer =>
         <div key={singleAnswer.country} className={styles.singleRow}>
-          <div className={styles.col1Layout} data-testid={`${singleAnswer.country}-col1-record`}>
+          <div className={`${styles.col1} ${styles.col1Layout}`} 
+            data-testid={`${singleAnswer.country}-col1-record`}>
             {singleAnswer.country}
           </div>
-          <div className={styles.col2Layout} data-testid={`${singleAnswer.country}-col2-record`}>
+          <div className={`${styles.col2} ${styles.col2Layout}`}
+            data-testid={`${singleAnswer.country}-col2-record`}>
             {singleAnswer.capital}
           </div>
-          <div className={styles.col3Layout} data-testid={`${singleAnswer.country}-col3-record`}>
+          <div className={`${styles.col3} ${styles.col3Layout}`} 
+            data-testid={`${singleAnswer.country}-col3-record`}>
             {singleAnswer.timeSpent}
           </div>
         </div>
