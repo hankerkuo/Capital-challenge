04798db938fd0027d12229c6b6a97cca7e577bd8
Hank <hankerkuo@gmail.com>
2022-11-13 20:45:13 +0800

    remove unnecessary useEffect, make game update before render

--- src/components/CapitalMainWidget.tsx
+++ src/components/CapitalMainWidget.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useRef, useReducer, useState } from 'react';
+import { useRef, useReducer, useState } from 'react';
 import type { ChangeEvent } from 'react';
 
 import QuestAndHint from 'src/components/answerForm/QuestAndHint';
 import AnswerInput from 'src/components/answerForm/AnswerInput';
 import StartButton from 'src/components/answerForm/StartButton';
 import AnswerRecord from 'src/components/dashboard/AnswerRecord';
-import PopupNotification from './notification/PopupNotification';
+import PopupNotification from 'src/components/notification/PopupNotification';
 
 import useTimer from 'src/hooks/useTimer';
 import QuestStateReducer from 'src/reducer/QuestStateReducer';
-import { QuestActionType } from 'src/reducer/QuestStateReducer';
+import { QuestActionEnum } from 'src/reducer/QuestStateReducer';
 
 import styles from 'src/styles/components/CapitalMainWidget.module.css'
 
@@ -29,7 +29,7 @@ const CapitalMainWidget = ({ countries }: TCapitalMainWidgetProps) => {
     quest: { country: '', capital: [''] },
     answerRecord: [],
     gameOngoing: false,
-    remainQuest: countries
+    remainQuest: {}
   });
 
   const actions = {
@@ -41,24 +41,15 @@ const CapitalMainWidget = ({ countries }: TCapitalMainWidgetProps) => {
     setPause
   }
 
-  useEffect(() => {
-    if (Object.keys(questState.remainQuest).length > 0) {
-      gameUpdate({ type: QuestActionType.NEXT_QUEST, ...actions });
-    } else {
-      gameUpdate({ type: QuestActionType.QUEST_END, ...actions });
-      setNotifyGameEnd((prev) => !prev);
-    }
-  }, [questState.remainQuest]);
-
   const startNewGame = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.currentTarget.textContent = 'Game started';
-    gameUpdate({ type: QuestActionType.START, ...actions });
+    gameUpdate({ type: QuestActionEnum.START, ...actions });
   }
 
   const checkAnswer = (e: ChangeEvent<HTMLInputElement>) => {
     // initialize the game on first typing
     if (!questState.gameOngoing) {
-      gameUpdate({ type: QuestActionType.TYPE_TO_START, ...actions });
+      gameUpdate({ type: QuestActionEnum.TYPE_TO_START, ...actions });
     }
     let answerMatched = false;
     questState.quest.capital.forEach(capitalName => {
@@ -67,14 +58,23 @@ const CapitalMainWidget = ({ countries }: TCapitalMainWidgetProps) => {
       }
     });
     if (answerMatched) {
-      gameUpdate({ type: QuestActionType.ANSWER_MATCHED, ...actions });
+      gameUpdate({ type: QuestActionEnum.ANSWER_MATCHED, ...actions });
+      // the quest amount state will be updated in the next render
+      // here need to get the REAL remaining quests by minus 1
+      if (Object.keys(questState.remainQuest).length - 1 > 0) {
+        gameUpdate({ type: QuestActionEnum.NEXT_QUEST, ...actions });
+      } else {
+        gameUpdate({ type: QuestActionEnum.QUEST_END, ...actions });
+        setNotifyGameEnd((prev) => !prev);
+      }
     }
   }
 
   return (
     <div className={styles.mainWidgetLyt}>
       <div className={styles.leftRegionLyt}>
-        <QuestAndHint quest={questState.quest} timer={timer} />
+        <QuestAndHint quest={questState.quest} 
+          timer={timer} showBlur={Object.keys(questState.remainQuest).length === 0}/>
         <AnswerInput {...{ inputEle, checkAnswer }} />
         <StartButton {...{ questState, startNewGame, timer }} />
       </div>
