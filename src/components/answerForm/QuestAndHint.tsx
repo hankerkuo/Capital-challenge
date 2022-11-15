d67debc795e64d5ef9c2fee0cb43a993172d09ed
Hank <hankerkuo@gmail.com>
2022-11-15 20:10:07 +0800

    fix bug for not hiding hint after answering the last question

--- src/components/answerForm/QuestAndHint.tsx
+++ src/components/answerForm/QuestAndHint.tsx
@@ -21,8 +21,8 @@ const HintChar = ({ capital, idx }: { capital: string, idx: number }) => {
 
 // TODO: add more flexbility to the function for different interval
 // possible for user to choose lower interval and maybe typing challenge mode?
-const QuestAndHint = ({ quest, timer, showBlur }: 
-    { quest: TQuestObj, timer: number, showBlur: boolean }) => {
+const QuestAndHint = ({ quest, timer, showBlur }:
+  { quest: TQuestObj, timer: number, showBlur: boolean }) => {
   // hint provide to gamer
   const [hintIdx, setHintIdx] = useState<number>(0);
 
@@ -55,14 +55,16 @@ const QuestAndHint = ({ quest, timer, showBlur }:
         <p className={`${styles.hintCharBlur} ${styles.hintCharLyt} ${styles.titlePLyt}`}>
           Quest placeholder
         </p> :
-        <p className={`${styles.hintChar} ${styles.hintCharLyt} ${styles.titlePLyt}`}>
-          {`${quest.country}:`}
-        </p>
+        <>
+          <p className={`${styles.hintChar} ${styles.hintCharLyt} ${styles.titlePLyt}`}>
+            {`${quest.country}:`}
+          </p>
+          {[...Array(hintIdx)].map((x, i) =>
+            // key here: e.g., ['kyiv', 'kiev'] will become 'kyiv,kiev'
+            <HintChar capital={aggregatedCapital} idx={i} key={`${quest.capital}${i}`} />
+          )}
+        </>
       }
-      {[...Array(hintIdx)].map((x, i) =>
-        // key here: e.g., ['kyiv', 'kiev'] will become 'kyiv,kiev'
-        <HintChar capital={aggregatedCapital} idx={i} key={`${quest.capital}${i}`} />
-      )}
     </div>
   )
 }
