ead629466c6ee8df77ffc6adfcbf916bf465a79a
Hank <hankerkuo@gmail.com>
2022-12-04 23:28:29 +0800

    fix linting

--- src/utils/RandomUtil.ts
+++ src/utils/RandomUtil.ts
@@ -8,16 +8,16 @@ class RandomUtil {
     return array[randomIdx];
   }
 
-  static getOneKeyValueFromObj = 
-    <T>(target: { [key: string]: T }): { key: string; value: T } | null=> {
-    const randomKey = RandomUtil.getOneFromArray(Object.keys(target));
-    return randomKey ? {
-      key: randomKey,
-      value: target[randomKey]
-    }: null
-  }
+  static getOneKeyValueFromObj =
+    <T>(target: { [key: string]: T }): { key: string; value: T } | null => {
+      const randomKey = RandomUtil.getOneFromArray(Object.keys(target));
+      return randomKey ? {
+        key: randomKey,
+        value: target[randomKey]
+      } : null
+    }
 
-  static getXFromArray = <T>(array: T[], targetX: number): T[] | null=> {
+  static getXFromArray = <T>(array: T[], targetX: number): T[] | null => {
     const length = array.length;
     if (length === 0) return null;
     if (targetX <= 0) throw Error("target random amount should be > 0");
@@ -25,11 +25,11 @@ class RandomUtil {
     const startIdx = Math.floor(Math.random() * length);
     // pick a interval which won't exceed array length
     let interval = Math.floor(Math.random() * Math.floor(length / targetX));
-    interval = interval === 0 ? 1: interval;
+    interval = interval === 0 ? 1 : interval;
     const result: T[] = [];
-    for(let i = 0; i < targetX; i++) {
+    for (let i = 0; i < targetX; i++) {
       let idx = startIdx + interval * i;
-      idx = idx < length? idx: idx - length;
+      idx = idx < length ? idx : idx - length;
       result.push(array[idx]);
     }
     return result;
