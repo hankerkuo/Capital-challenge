f71387e09e757f359c6d3656d9fdfc330c3fb286
Hank <hankerkuo@gmail.com>
2022-12-23 19:13:53 +0800

    adjust spinning icon and styles

--- src/components/answerForm/StartButton.tsx
+++ src/components/answerForm/StartButton.tsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCircleNotch,
+} from "@fortawesome/free-solid-svg-icons";
+
 import type { TQuestState } from 'src/reducer/QuestStateReducer';
 
 import styles from 'src/styles/components/answerForm/StartButton.module.css'
@@ -8,13 +13,22 @@ const StartButton = (
       questState: TQuestState,
       startNewGame: (e: React.MouseEvent<HTMLButtonElement>) => void
       timer: number
+      pending: boolean
     }) => {
+  let buttonCss = `${styles.newGameButtonLyt} ${styles.newGameButton}`;
+  props.pending ? buttonCss = `${buttonCss} ${styles.buttonDisabled}` : buttonCss;
   return (
-    <button data-testid='start-btn-and-timer'
-      className={`${styles.newGameButtonLyt} ${styles.newGameButton}`}
-      onClick={props.startNewGame}>
-      {props.questState.gameOngoing ? props.timer : 'Start Game'}
-    </button>
+    <div className={`${styles.buttonContainerLyt}`}>
+      {/* TODO: let spinning apears only when data is loading */}
+      <FontAwesomeIcon className={`${styles.spinnerAdjust}`}
+        icon={faCircleNotch} spin={true} />
+      <button data-testid='start-btn-and-timer'
+        className={buttonCss}
+        onClick={props.startNewGame}>
+        {props.questState.gameOngoing ? props.timer : 'Start Game'}
+      </button>
+    </div>
+
   )
 }
 
