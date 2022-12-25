a4ea79416171181cb40b25c69e71b4a1c6126d8b
Hank <hankerkuo@gmail.com>
2022-12-25 20:13:46 +0800

    refine logging aspect class

--- src/aop/LoggingAspect.aop.ts
+++ src/aop/LoggingAspect.aop.ts
@@ -2,8 +2,23 @@ import { Aspect, AspectContext } from 'ts-aspect';
 
 export default class LoggingAspect implements Aspect {
   execute(ctx: AspectContext): any {
+
     delete ctx.returnValue;
+    let metaData = null;
+    try {
+      metaData = JSON.stringify(ctx);
+    } catch(e) {
+      // incase circular reference appearing in ctx object
+      if (e instanceof TypeError) {
+        console.log('[Aspect logging Error] \
+          Failed trasforming context to json, log function name only');
+        // console.error(e);
+        metaData = JSON.stringify(ctx.methodName);
+      } else {
+        console.error(e);
+      }
+    }
     console.log(`[Aspect logging] ${new Date().toJSON()} \
-      Function call meta data: ${JSON.stringify(ctx)}`);
+      Function call meta data: ${metaData}`);
   }
 }
