5526a2de2de8cd21f57d14dede5910c2963ed1df
Hank <hankerkuo@gmail.com>
2023-01-01 22:54:02 +0800

    add login UI

---
+++ src/components/account/LoginWrapper.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import styles from "src/styles/components/account/LoginWrapper.module.css";
+
+import LoginButton from "src/components/account/LoginButton";
+import LoginForm from "src/components/account/LoginForm";
+
+const CurrentUser = ({ user }: { user: string }) => {
+  return (
+    <span className={`${styles.currentUserFont} ${styles.currentUserLyt}`}>
+      Hi! {user}
+    </span>
+  );
+};
+const LoginWrapper = ({ user }: { user: string }) => {
+  const [showHideLoginForm, setShowHideLoginForm] = useState(false);
+  const handleLoginButton = () => {
+    setShowHideLoginForm(showHideLoginForm => !showHideLoginForm);
+  }
+  const closeLoginForm = () => {
+    setShowHideLoginForm(false);
+  }
+  return (
+    <>
+      <div className={`${styles.containerLyt}`}>
+        <CurrentUser user={user} />
+        <LoginButton handleLoginButton={handleLoginButton}/>
+      </div>
+      {showHideLoginForm ? <LoginForm closeLoginForm={closeLoginForm}/>: null}
+    </>
+  );
+};
+
+export default LoginWrapper;
