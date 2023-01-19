e509c4e83f0a5b640e1b5d43066c6220e98d957d
Hank <hankerkuo@gmail.com>
2023-01-19 21:08:01 +0800

    add clean up of render after each test

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,15 @@
 // src/setupTests.js
 import { server } from 'src/mocks/server'
+import { cleanup } from '@testing-library/react'
 
 // Establish API mocking before all tests.
 beforeAll(() => server.listen())
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
-afterEach(() => server.resetHandlers())
+afterEach(() => {
+  server.resetHandlers();
+  // clean up rendered components in each test
+  cleanup();
+})
 // Clean up after the tests are finished.
 afterAll(() => server.close())
