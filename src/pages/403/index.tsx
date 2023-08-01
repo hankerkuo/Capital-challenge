da392e4aa7bd9967b100510584760258c4434dd2
Hank <hankerkuo@gmail.com>
2023-08-02 00:05:00 +0800

    add access denied page

---
+++ src/pages/403/index.tsx
@@ -0,0 +1,7 @@
+import styles from 'src/styles/ImageTagging.module.css';
+
+const AccessDenied = () => {
+  return <div className={`${styles.font}`}>Access Denied</div>;
+}
+
+export default AccessDenied;
